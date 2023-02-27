309047ef04661a8bbaa0d378aa64a9cbdbb033ab
Miguel GitHub <114403299+MiguelHidalgoCastro@users.noreply.github.com>
2023-02-27 09:09:56 +0100

    fin reto 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { FavoritasComponent } from './favoritas/favoritas.component';
+import { CreditosComponent } from './creditos/creditos.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'favoritos', component: FavoritasComponent },
+  { path: 'creditos', component: CreditosComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
