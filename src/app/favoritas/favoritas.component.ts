f5d47ef303e523321b43eb02dc854bddf0d64732
Miguel GitHub <114403299+MiguelHidalgoCastro@users.noreply.github.com>
2023-02-27 08:47:48 +0100

    componentes añadidos

---
+++ src/app/favoritas/favoritas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favoritas',
+  templateUrl: './favoritas.component.html',
+  styleUrls: ['./favoritas.component.css']
+})
+export class FavoritasComponent {
+
+}
